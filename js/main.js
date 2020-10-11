d801b8157d5c8dcab85e1aa458bf431750ddd958
Naphtali Rubin <naphtali.r.89@gmail.com>
2020-10-11 15:48:50 +0300

    Added a render Modal feature instead of html modals

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ function renderPortItem() {
     var items = getPortItems();
     var currCount = 0;
     var strHTML = items.map(function(item) {
-        return `<div class="col-md-4 col-sm-6 portfolio-item">
-                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${++currCount}">
+        return `<div class="col-md-4 col-sm-6 portfolio-item item${++currCount}">
+                    <a class="portfolio-link" data-toggle="modal" onclick="renderPortModal('${currCount}')" href="#portfolioModal">
                         <div class="portfolio-hover">
                             <div class="portfolio-hover-content">
                                 <i class="fa fa-plus fa-3x"></i>
@@ -27,4 +27,32 @@ function renderPortItem() {
     })
     var elPortItems = document.querySelector('.row1');
     elPortItems.innerHTML = strHTML.join('');
+}
+
+function renderPortModal(num) {
+    var strHTML = `
+                <div class="row">
+                    <div class="col-lg-8 mx-auto">
+                        <div class="modal-body">
+                            <!-- Project Details Go Here -->
+                                <h2>Project Name</h2>
+                                <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                                <img class="img-fluid d-block mx-auto" src="img/portfolio/0${num}-full.jpg" alt="">
+                                <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate,
+                                maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                                <ul class="list-inline">
+                                    <li>Date: January 2017</li>
+                                    <li>Client: Threads</li>
+                                    <li>Category: Illustration</li>
+                                </ul>
+                                <button class="btn btn-primary" data-dismiss="modal" type="button">
+                    <i class="fa fa-times"></i>
+                    Close Project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+    var elModal = document.querySelector('.modal-container');
+    console.log('elmodal is:', elModal)
+    elModal.innerHTML = strHTML;
 }
