e6117781e060e96106a6339c413d69d9af1b25fc
Naphtali Rubin <naphtali.r.89@gmail.com>
2020-10-11 14:44:14 +0300

    Changed the portfolio items section to be JS rendered

--- js/main.js
+++ js/main.js
@@ -3,10 +3,28 @@
 console.log('Starting up');
 
 function initPortPage() {
-
+    renderPortItem();
 }
 
 function renderPortItem() {
-    var item = getPortItem();
-
+    var items = getPortItems();
+    var currCount = 0;
+    var strHTML = items.map(function(item) {
+        return `<div class="col-md-4 col-sm-6 portfolio-item">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${++currCount}">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img class="img-fluid" src="img/portfolio/0${currCount}-thumbnail.jpg" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                        <h4>${item.title}</h4>
+                        <p class="text-muted">${item.desc}</p>
+                    </div>
+                </div>`;
+    })
+    var elPortItems = document.querySelector('.row1');
+    elPortItems.innerHTML = strHTML.join('');
 }
--- js/services/portfolio-service.js
+++ js/services/portfolio-service.js
@@ -1,8 +1,61 @@
 'use strict';
 
-var gPortItems;
+var gPortItems = [{
+        id: "minesweeper",
+        name: "minesweeper",
+        title: "My Mine-sweeper",
+        desc: "My first fully functional web page based game.",
+        url: "projs/minesweeper",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: "bookshop",
+        name: "bookshop",
+        title: "My Bookshop",
+        desc: "A book table for a bookshop web page.",
+        url: "projs/bookshop",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    }, {
+        id: "minesweeper",
+        name: "minesweeper",
+        title: "My Mine-sweeper",
+        desc: "My first fully functional web page based game.",
+        url: "projs/minesweeper",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: "bookshop",
+        name: "bookshop",
+        title: "My Bookshop",
+        desc: "A book table for a bookshop web page.",
+        url: "projs/bookshop",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    }, {
+        id: "minesweeper",
+        name: "minesweeper",
+        title: "My Mine-sweeper",
+        desc: "My first fully functional web page based game.",
+        url: "projs/minesweeper",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: "bookshop",
+        name: "bookshop",
+        title: "My Bookshop",
+        desc: "A book table for a bookshop web page.",
+        url: "projs/bookshop",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    }
+
+];
 var gModals;
 
-function getPortItem() {
-
+function getPortItems() {
+    return gPortItems;
 }
